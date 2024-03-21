4a58b66278904389731f82e65408af3f08a4de69
li1553770945 <1553770945@qq.com>
2024-03-22 00:09:29 +0800

    feat:增加决赛题解

--- supply-depot/generator.cpp
+++ supply-depot/generator.cpp
@@ -6,15 +6,24 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     registerGen(argc, argv, 1);
-    for(int test_index = 1; test_index <= 20; test_index++)
+    for(int test_index = 1; test_index <= 10; test_index++)
     {
         startTest(test_index);
         int T = rnd.next(8, 10);
         cout << T << endl;
 
-        for(int i=0;i<T;i++)
+        for(int t=0;t<T;t++)
         {
-            int n = rnd.next(4'0'000, 5'0'000);
+
+            int n;
+            if(test_index <=5)
+            {
+                n = rnd.next(4000, 5000);
+            }
+            else
+            {
+                n = rnd.next(40000, 50000);
+            }
             cout<<n<<endl;
             for(int i=1;i<=n;i++)
             {
@@ -27,10 +36,17 @@ int main(int argc, char* argv[])
             cout<<endl;
 
 
-            int m = rnd.next(4'0'000, 5'0'000);
-            cout << m << endl;
-
-            for(int i=1;i<=m;i++)
+            int m;
+            if(test_index <=5)
+            {
+                m = rnd.next(4000, 5000);
+            }
+            else
+            {
+                m = rnd.next(40000, 50000);
+            }
+            cout<<m<<endl;
+            for(int t=1;t<=m;t++)
             {
                 int l = rnd.next(1,n);
                 int r = rnd.next(l,n);
